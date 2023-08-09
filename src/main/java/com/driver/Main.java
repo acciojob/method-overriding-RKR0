92702902daca87c45f396ab7c57b192303ba3f0f
RKR0 <114342990+RKR0@users.noreply.github.com>
2023-08-09 21:26:37 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,22 +2,22 @@ package com.driver;
 
 
 
-class A{
+
+
+
+
+public class Main {
+  public static class A{
 	public String meth() {
 		return "Invoking method from class A";
 	}
 }
 
-class B extends A{
+public static class B extends A{
 	public String meth() {
 		return "Method is overridden in Extendend class B";
 	}
 }
-
-
-
-public class Main {
-  
 	public static void main(String[] args) {
 		
 		B obj = new B();
